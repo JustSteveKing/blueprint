38839c6c6339eef98dc3eb7e8297d38915b25022
Steve McDougall <juststevemcd@gmail.com>
2022-11-11 11:56:17 +0000

    Adding a few additions to the FIle Mixin

--- src/FileMixins.php
+++ src/FileMixins.php
@@ -1,18 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Blueprint;
 
+use Closure;
+use Illuminate\Filesystem\Filesystem;
+
+/**
+ * @mixin Filesystem
+ */
 class FileMixins
 {
-    private $stubs = [];
+    /**
+     * @var array<string,string>
+     */
+    private array $stubs = [];
 
-    public function stub()
+    public function stub(): Closure
     {
-        return function ($path) {
+        return function ($path): string {
             if (!isset($this->stubs[$path])) {
                 $stubPath = file_exists($customPath = CUSTOM_STUBS_PATH . '/' . $path)
                           ? $customPath
                           : STUBS_PATH . '/' . $path;
+
                 $this->stubs[$path] = $this->get($stubPath);
             }
 
